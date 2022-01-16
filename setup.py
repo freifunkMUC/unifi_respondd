d3d08fd63d6bec7f70b0b59bf2fab20724c2aa8c
GoliathLabs <git@xdfr.de>
2022-01-16 22:57:45 +0100

    Run black

--- setup.py
+++ setup.py
@@ -19,5 +19,10 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     include_package_data=True,
-    install_requires=["pyunifi==2.21", "geopy==2.2.0", "pyyaml==6.0", "dataclasses_json==0.5.6"],
+    install_requires=[
+        "pyunifi==2.21",
+        "geopy==2.2.0",
+        "pyyaml==6.0",
+        "dataclasses_json==0.5.6",
+    ],
 )
