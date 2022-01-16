61853e5554b366fb92fe2df21f91bf23847059b0
Annika Wickert <awlx@users.noreply.github.com>
2022-01-16 12:19:22 +0000

    Run black

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "unifi_respondd",
-    version = "0.0.5",
-    author = "Annika Wickert",
-    author_email = "aw@awlnx.space",
-    description = ("A tool to display Unifi APs on Freifunk maps."),
-    license = "GPLv3",
-    keywords = "Unifi Freifunk",
-    url = "http://packages.python.org/unifi_respondd",
-    packages=['unifi_respondd'],
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
+    name="unifi_respondd",
+    version="0.0.5",
+    author="Annika Wickert",
+    author_email="aw@awlnx.space",
+    description=("A tool to display Unifi APs on Freifunk maps."),
+    license="GPLv3",
+    keywords="Unifi Freifunk",
+    url="http://packages.python.org/unifi_respondd",
+    packages=["unifi_respondd"],
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     include_package_data=True,
 )
